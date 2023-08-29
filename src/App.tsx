11695bd4e5dfc1ed7a7ade53d8893d07e61c9cfd
stereobooster <stereobooster@gmail.com>
2023-08-29 20:42:21 +0200

    rnw seems to work

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Platform, Text } from "react-native";
+
+export default function App() {
+    return (
+        <Text>Hello World! Your platform is { Platform.OS }</Text>
+    )
+}
